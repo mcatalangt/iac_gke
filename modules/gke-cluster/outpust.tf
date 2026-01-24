6cb01550b5cce1cbb04cc0508658bac1d00abfa8
MarcoTulioGT <devopsgt@gmail.com>
2026-01-23 18:15:35 -0600

    all

--- modules/gke-cluster/outpust.tf
+++ modules/gke-cluster/outpust.tf
@@ -7,14 +7,14 @@ output "endpoint" {
 output "host" {
   description = "El endpoint del cluster GKE"
   sensitive   = true
-  value       = google_container_cluster.primary.endpoint 
+  value       = google_container_cluster.gke_cluster.endpoint 
   # (Asegúrate que tu recurso se llame google_container_cluster.primary o cambia el nombre aquí)
 }
 
 output "cluster_ca_certificate" {
   description = "Certificado CA público del cluster"
   sensitive   = true
-  value       = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  value       = google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate
 }
 
 output "token" {
