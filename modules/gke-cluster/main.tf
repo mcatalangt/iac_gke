cb02d9744b711c7691d4720eead11e0e66f96346
MarcoTulioGT <devopsgt@gmail.com>
2026-02-02 15:10:19 -0600

    hola

--- modules/gke-cluster/main.tf
+++ modules/gke-cluster/main.tf
@@ -33,7 +33,7 @@ resource "google_container_node_pool" "primary_nodes" {
   location   = var.region
   cluster    = google_container_cluster.gke_cluster.name
 
-  node_count = 2
+  node_count = 1
 
   node_config {
     machine_type = "e2-standard-4"
