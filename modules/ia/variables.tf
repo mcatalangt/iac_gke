942f97049faaf0e3d4f921ea6779809880612938
MarcoTulioGT <devopsgt@gmail.com>
2026-02-02 19:01:03 -0600

    feat: agregando modulo de ia con qdrant y ajustes en k8s

--- modules/ia/variables.tf
+++ modules/ia/variables.tf
@@ -1,3 +1,8 @@
 variable "cluster_endpoint" {}
 variable "access_token" {}
 variable "cluster_ca_certificate" {}
+variable "qdrant_api_key" {
+  description = "La clave secreta para proteger la base de datos Qdrant"
+  type        = string
+  sensitive   = true 
+}
