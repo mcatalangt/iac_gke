590c7c9b9344cfe600fcb36b7daa4efe2a12b128
MarcoTulioGT <devopsgt@gmail.com>
2026-02-03 09:35:06 -0600

    feat: agregando modulo de ia con qdrant y ajustes en k8s

--- modules/ia/providers.tf
+++ modules/ia/providers.tf
@@ -18,5 +18,9 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  
+  kubernetes {
+    host                   = "https://$${var.cluster_endpoint}"
+    token                  = var.token
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+  }
 }
