f3af519d6d3e9a205b13baa37786d437958d581d
MarcoTulioGT <devopsgt@gmail.com>
2026-02-20 11:50:41 -0600

    Add new features host var 2

--- modules/ia/providers.tf
+++ modules/ia/providers.tf
@@ -12,14 +12,14 @@ terraform {
 }
 
 provider "kubernetes" {
-  host                   = "https://$${var.cluster_endpoint}"
+  cluster_endpoint       = "https://$${var.cluster_endpoint}"
   token                  = var.token
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes = {
-    host                   = "https://$${var.cluster_endpoint}"
+    cluster_endpoint       = "https://$${var.cluster_endpoint}"
     token                  = var.token
     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
   }
