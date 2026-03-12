e147ff1b24b9ccdce983560359259342ae1442f1
MarcoTulioGT <devopsgt@gmail.com>
2026-03-11 18:57:19 -0600

    Add New modules - Grafana node graphs configs

--- modules/gke-cluster/main.tf
+++ modules/gke-cluster/main.tf
@@ -39,8 +39,6 @@ resource "google_container_node_pool" "primary_nodes" {
     machine_type = "e2-standard-4"
     disk_size_gb = 20
 
-    spot = true
-
     labels = {
       carga = "general"
     }
